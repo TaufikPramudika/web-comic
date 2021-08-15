447d2661889dace379bdec6bb3a894730d3234cf
Novan_ <83200466+nov4n-4r@users.noreply.github.com>
2021-08-15 02:31:57 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const myPlugin = require('./root/asset/plugin/node-plugin.js');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.get('/', (req, res) => {  
+  res.sendFile(path.join(`${__dirname}/public/index.html`))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
